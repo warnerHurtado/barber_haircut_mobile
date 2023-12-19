621fbbec6542a993220a4c8b797815211000e020
warner hurtado laguna <warner3898@gmail.com>
2023-12-19 14:22:28 -0600

    feat: create shop screen

--- lib/features/shop/domain/datasources/shop_datasource.dart
+++ lib/features/shop/domain/datasources/shop_datasource.dart
@@ -3,5 +3,5 @@ import 'package:barber_haircut_mobile/features/shop/domain/entities/shop.dart';
 abstract class ShopDatasource {
   Future<List<Shop>> getShopsByPage({int limit = 10, int skip = 0});
 
-  Future<Shop> getShopById(String id);
+  Future<Shop> getShopById(String shopId);
 }
--- lib/features/shop/presentation/providers/providers.dart
+++ lib/features/shop/presentation/providers/providers.dart
@@ -1,2 +1,3 @@
 export './shop_repository_provider.dart';
 export './shops_provider.dart';
+export './shop_provider.dart';
